b0b8ca315a3412a1178b8c1029afe6f9999ce24e
berger <berger@localhost>
2008-12-16 09:38:51 +0000

    Updated for new Webpage

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mvn clean &&\
 mvn package install assembly:assembly &&\
-cp wmsx/target/*distribution* $HOME/public_html/wmsx/ &&\
-cp target/*src*zip $HOME/public_html/wmsx/
+scp wmsx/target/*distribution* ochsner.dps.uibk.ac.at:/home/trac/www/download/ &&\
+scp target/*src*zip ochsner.dps.uibk.ac.at:/home/trac/www/download/
