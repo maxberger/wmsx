d30746ffc2c1cdeee83e709c0eec979a31cf6c53
Max Berger <max@berger.name>
2008-12-22 11:42:52 +0100

    updated for new wmsx directory

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mvn clean &&\
-mvn package install assembly:assembly &&\
-scp wmsx/target/*distribution* ochsner.dps.uibk.ac.at:/home/trac/www/download/ &&\
-scp target/*src*zip ochsner.dps.uibk.ac.at:/home/trac/www/download/
+mvn -o package install assembly:assembly &&\
+scp wmsx/target/*distribution* ochsner.dps.uibk.ac.at:/home/wmsx/www/download/ &&\
+scp target/*src*zip ochsner.dps.uibk.ac.at:/home/wmsx/www/download/
