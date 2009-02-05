cb40923912f83af4031addcef15c64389dbf86ac
Max Berger <max@berger.name>
2009-02-05 15:16:59 +0100

    SVN Props are no longer needed with mercurial

--- svnproperties.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-find . -name \*.java -exec svn ps svn:keywords "Revision Date Id" '{}' \;
-find . -name \*.java -exec svn ps svn:eol-style native '{}' \;
-find . -name \*.xsl  -exec svn ps svn:eol-style native '{}' \;
-find . -name \*.xml  -exec svn ps svn:eol-style native '{}' \;
