b43a2bf63273ae5423b1a96e960e20732e02bb7c
bafu <bafu@illi>
2009-03-18 01:40:48 +0100

    [no commit message]

---
+++ src/main/java/at/ac/uibk/dps/wmsxgui/App.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package at.ac.uibk.dps.wmsxgui;
+
+/**
+ *
+ * @author bafu
+ */
+public class App {
+
+    public static void main(final String[] args) {
+        System.out.println("WmsxGUI is starting...");
+        new WmsxMainWindow();
+    }
+}
+jeuclid-testsuite/index.html
