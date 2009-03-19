df3230f08e854d09f11aac8c8a6895c5daf549fe
bafu <bafu@illi>
2009-03-19 15:37:50 +0100

    -

---
+++ src/main/java/at/ac/uibk/dps/wmsxgui/business/BusinessManager.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package at.ac.uibk.dps.wmsxgui.business;
+
+/**
+ *
+ * @author bafu
+ */
+public class BusinessManager {
+
+}
