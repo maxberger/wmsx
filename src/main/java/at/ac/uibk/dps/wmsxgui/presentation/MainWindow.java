32a9c36f9d9a4ae4e9286d3f9cfb94f1527450ab
bafu <bafu@illi>
2009-03-25 14:24:00 +0100

    [no commit message]

--- src/main/java/at/ac/uibk/dps/wmsxgui/presentation/MainWindow.java
+++ src/main/java/at/ac/uibk/dps/wmsxgui/presentation/MainWindow.java
@@ -37,11 +37,11 @@ public class MainWindow extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 637, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 461, Short.MAX_VALUE)
         );
 
         pack();
