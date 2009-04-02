127df33b1f2b55e23ed1a2710821a60ebeb24c00
bafu <bafu@illi>
2009-04-02 02:51:38 +0200

    Requestor wurde erstellt und somit die Verbindung zum Provider hergestellt.

---
+++ src/main/java/at/ac/uibk/dps/wmsxgui/business/BusinessManager.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package at.ac.uibk.dps.wmsxgui.business;
+
+/**
+ *
+ * @author bafu
+ */
+public class BusinessManager {
+
+    private Requestor requestor;
+
+    /* Singleton Pattern */
+	private BusinessManager()
+	{
+        requestor = Requestor.getInstance();
+    }/** Private innere statische Klasse, realisiert Singleton Pattern
+	 *
+	 */
+	private static class SingletonHolder
+	{
+		private final static BusinessManager INSTANCE = new BusinessManager();
+	}
+
+	/** Gibt immer die gleiche Instanz zurück
+	 * @return Instance der Game Klasse
+	 */
+	public static BusinessManager getInstance()
+	{
+		return SingletonHolder.INSTANCE;
+	}
+	/* Singleton */
+
+    public Requestor getRequestor()
+    {
+        return requestor;
+    }
+}
