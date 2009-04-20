3c18f434b886a4eb4cd6700aa05cbaf95d669f33
lancelot <lancelot@Master>
2009-04-20 10:32:25 +0200

    [no commit message]

---
+++ src/main/java/at/ac/uibk/dps/wmsxgui/App.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package at.ac.uibk.dps.wmsxgui;
+
+import at.ac.uibk.dps.wmsxgui.presentation.*;
+import at.ac.uibk.dps.wmsxgui.business.*;
+/**
+ *
+ * @author bafu
+ */
+public class App {
+
+    /**
+    * @param args the command line arguments
+    */
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+
+
+                System.out.println("wmsx-gui is starting...");
+                new MainWindow(BusinessManager.getInstance()).setVisible(true);
+
+            }
+        });
+    }
+}
