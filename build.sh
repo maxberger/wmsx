255c66fa081c6a2ccb5c32df3106a8cbab9feb58
Max Berger <max@berger.name>
2009-07-10 11:13:27 +0200

    Removed obsolete shell scripts

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mvn clean &&\
-mvn -o package install assembly:assembly &&\
-scp wmsx/target/*distribution* ochsner.dps.uibk.ac.at:/home/wmsx/www/download/ &&\
-scp target/*src*zip ochsner.dps.uibk.ac.at:/home/wmsx/www/download/
